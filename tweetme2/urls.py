8144f8985a7bd7ddb469ebc2e39227611df1de23
ahmed8047762 <an8047762@gmail.com>
2024-11-29 17:24:15 +0500

    User profile added

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from accounts.views import (
     login_view,
@@ -26,8 +26,7 @@ from accounts.views import (
 
 from tweets.views import (
     tweets_list_view,
-    tweets_detail_view, 
-    tweets_profile_view
+    tweets_detail_view,
 )
 
 urlpatterns = [
@@ -37,7 +36,7 @@ urlpatterns = [
     path('logout/', logout_view),
     path('register/', register_view),
     path('<int:tweet_id>', tweets_detail_view),
-    path('profiles/<str:username>', tweets_profile_view),
+    re_path(r'profiles?/', include('profiles.urls')),
     path('api/tweets/', include('tweets.api.urls')),
 ]
 
