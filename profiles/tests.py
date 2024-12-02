f8077c18a2cc2fef9dce2eae20f68375b6178b94
ahmed8047762 <an8047762@gmail.com>
2024-12-02 17:35:48 +0500

    Add followers view

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,7 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
-from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
 from .models import Profile
 
 User = get_user_model()
@@ -10,6 +10,11 @@ class ProfileTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='cfe', password='1234')
         self.user2 = User.objects.create_user(username='cfe2', password='1234')
+    
+    def get_client(self):
+        client = APIClient()
+        client.login(username=self.user.username, password='1234')
+        return client
     
     def test_profile_created(self):
         qs = Profile.objects.all()
@@ -24,3 +29,42 @@ class ProfileTestCase(TestCase):
         first_user_following_no_one = first.following.all()
         self.assertTrue(qs.exists())
         self.assertFalse(first_user_following_no_one.exists())
+        
+    def test_follow_api_endpoint(self):
+        client = self.get_client()
+        response = client.post(
+            f'/api/profiles/{self.user2.username}/follow',
+            {
+                "action": "follow"
+            }                       
+        )
+        r_data = response.json()
+        count = r_data.get("count")
+        self.assertEqual(count, 1)
+        
+    def test_unfollow_api_endpoint(self):
+        first = self.user
+        second = self.user2
+        first.profile.followers.add(second)
+        client = self.get_client()
+        response = client.post(
+            f'/api/profiles/{self.user2.username}/follow',
+            {
+                "action": "unfollow"
+            }                       
+        )
+        r_data = response.json()
+        count = r_data.get("count")
+        self.assertEqual(count, 0)
+        
+    def test_cannot_follow_api_endpoint(self):
+        client = self.get_client()
+        response = client.post(
+            f'/api/profiles/{self.user.username}/follow',
+            {
+                "action": "follow"
+            }                       
+        )
+        r_data = response.json()
+        count = r_data.get("count")
+        self.assertEqual(count, 0)
--- tweets/api/views.py
+++ tweets/api/views.py
@@ -78,13 +78,21 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
     return Response(serializer.data)
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def tweet_feed_view(request, *args, **kwargs):
+    user = request.user
+    qs = Tweet.objects.feed(user)
+    serializer = TweetSerializer(qs, many=True)
+    return Response(serializer.data, status=200)
+
+@api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
     username = request.GET.get('username')
     if username != None:
-        qs = qs.filter(user__username__iexact=username)
+        qs = qs.by_username(username)
     serializer = TweetSerializer(qs, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=200)
 
 def tweet_create_view_pure_django(request, *args, **kwargs):
     user = request.user
